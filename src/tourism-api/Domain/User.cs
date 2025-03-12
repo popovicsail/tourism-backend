7285d19b0f611705d464fc53ad4e90ed5785b84f
Anica Đukić <anica.djukic99@gmail.com>
2025-03-12 11:33:41 +0100

    Set role field as optional

--- src/tourism-api/Domain/User.cs
+++ src/tourism-api/Domain/User.cs
@@ -7,7 +7,7 @@ public class User
     public int Id { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
-    public string Role { get; set; }
+    public string? Role { get; set; }
 
     public bool IsValid()
     {
