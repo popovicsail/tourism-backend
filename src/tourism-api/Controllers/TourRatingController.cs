1029a7e6ddfa0d93e3546256a330b2bd92b058e3
aleksandar popovic <popovicsail@gmail.com>
2025-07-11 03:19:36 +0200

    feat: backend upgrade

---
+++ src/tourism-api/Controllers/TourRatingController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tourism_api.Domain;
+using tourism_api.Repositories;
+
+namespace tourism_api.Controllers;
+
+[Route("api/comments")]
+[ApiController]
+public class TourRatingController : ControllerBase
+{
+    private readonly TourRatingRepository _tourRatingRepo;
+
+    public TourRatingController(IConfiguration configuration)
+    {
+        _tourRatingRepo = new TourRatingRepository(configuration);
+    }
+
+
+
+    [HttpGet]
+    public ActionResult<List<TourRating>> GetAll()
+    {
+        try
+        {
+            List<TourRating> tourRatings = _tourRatingRepo.GetAll();
+            return Ok(tourRatings);
+        }
+        catch (Exception ex)
+        {
+            return Problem("An error occurred while fetching the comment.");
+        }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<List<TourRating>> GetById(int id, [FromQuery] string idType)
+    {
+        try
+        {
+            List<TourRating> tourRatings = _tourRatingRepo.GetById(id, idType);
+            return Ok(tourRatings);
+        }
+        catch (Exception ex)
+        {
+            return Problem("An error occurred while fetching the comment.");
+        }
+    }
+
+
+    [HttpPost]
+    public ActionResult<TourRating> Create([FromBody] TourRating tourRating)
+    {
+        if (tourRating.Comment == null)
+        {
+            tourRating.Comment = "";
+        }
+
+        try
+        {
+            TourRating createdTourRating = _tourRatingRepo.Create(tourRating);
+            return Ok(createdTourRating);
+        }
+        catch (Exception ex)
+        {
+            return Problem("An error occurred while creating the comment.");
+        }
+    }
+}
+
---
+++ src/tourism-api/Repositories/TourRatingRepository.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.Data.Sqlite;
+using tourism_api.Domain;
+
+namespace tourism_api.Repositories
+{
+    public class TourRatingRepository
+    {
+        private readonly string _connectionString;
+
+        public TourRatingRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration["ConnectionString:SQLiteConnection"];
+        }
+
+        public TourRating Create(TourRating tourRating)
+        {
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection(_connectionString);
+                connection.Open();
+                {
+                    string query = @"
+                        INSERT INTO TourRatings (UserId, TourId, RatingDate, Rating, Comment)
+                        VALUES (@UserId, @TourId, @RatingDate, @Rating, @Comment);
+                        SELECT last_insert_rowid();";
+
+                    using SqliteCommand command = new SqliteCommand(query, connection);
+                    command.Parameters.AddWithValue("@UserId", tourRating.UserId);
+                    command.Parameters.AddWithValue("@TourId", tourRating.TourId);
+                    command.Parameters.AddWithValue("@RatingDate", tourRating.RatingDate);
+                    command.Parameters.AddWithValue("@Rating", tourRating.Rating);
+                    command.Parameters.AddWithValue("@Comment", tourRating.Comment);
+
+                    int newId = Convert.ToInt32(command.ExecuteScalar());
+                }
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju SQL upita: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+                throw;
+            }
+
+            return tourRating;
+        }
+
+        public List<TourRating> GetById(int id, string idType)
+        {
+            List<TourRating> tourRatings = new List<TourRating>();
+
+            string searchQuery = null;
+
+            if (idType == "userId")
+            {
+                searchQuery = "WHERE t.UserId = @Id";
+            }
+            else if (idType == "tourId")
+            {
+                searchQuery = "WHERE t.TourId = @Id";
+            }
+
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection(_connectionString);
+                connection.Open();
+
+                string query = @$"
+                    SELECT t.Id, t.TourId, t.UserId, t.RatingDate, t.Rating, t.Comment, u.Username
+                    FROM TourRatings t 
+                    INNER JOIN Users u ON t.UserId = u.Id
+                    {searchQuery}";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Id", id);
+
+                using SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    tourRatings.Add(new TourRating
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        TourId = Convert.ToInt32(reader["TourId"]),
+                        UserId = Convert.ToInt32(reader["UserId"]),
+                        Comment = reader["Comment"].ToString(),
+                        Rating = Convert.ToInt32(reader["Rating"]),
+                        Username = reader["Username"].ToString()
+                    });
+                }
+
+                return tourRatings;
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+                throw;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+                throw;
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+                throw;
+            }
+        }
+
+        public List<TourRating> GetAll()
+        {
+            List<TourRating> tourRatings = new List<TourRating>();
+
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection(_connectionString);
+                connection.Open();
+
+                string query = @$"
+                    SELECT t.Id, t.TourId, t.UserId, t.RatingDate, t.Rating, t.Comment
+                    FROM TourRatings t";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+
+                using SqliteDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    tourRatings.Add(new TourRating
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        TourId = Convert.ToInt32(reader["TourId"]),
+                        UserId = Convert.ToInt32(reader["UserId"]),
+                        Comment = reader["Comment"].ToString(),
+                        Rating = Convert.ToInt32(reader["Rating"]),
+                    });
+                }
+
+                return tourRatings;
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+                throw;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+                throw;
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
